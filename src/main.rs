44356b373fc6d88a693e5c572995e78795571ae7
Jeff Kreeftmeijer <jeff@kreeft.me>
2025-01-20 19:40:41 +0100

    Set up a camera and diaplay a map
    Set up a 2D camera and render a map, which is a 100 x 100 pixel white block for
    now, rendered at 2X.

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,39 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (setup_camera, setup_map))
         .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2d::default());
+}
+
+fn setup_map(mut commands: Commands) {
+    let (size_x, size_y) = (100, 100);
+    let sprite_size = 2.;
+
+    commands
+        .spawn((
+            Transform::from_xyz(
+                -(size_x as f32 * sprite_size) / 2.,
+                -(size_y as f32 * sprite_size) / 2.,
+                0.,
+            ),
+            Visibility::default(),
+        ))
+        .with_children(|builder| {
+            for y in 0..=size_y {
+                for x in 0..=size_x {
+                    builder.spawn((
+                        Sprite {
+                            custom_size: Some(Vec2::splat(sprite_size)),
+                            color: Color::srgba(255., 255., 255., 255.),
+                            ..default()
+                        },
+                        Transform::from_xyz(sprite_size * x as f32, sprite_size * y as f32, 0.),
+                    ));
+                }
+            }
+        });
 }
