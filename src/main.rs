11cbeac09dc7ab3bb2480495d8a4de1f7fb18171
Jeff Kreeftmeijer <jeff@kreeft.me>
2025-01-20 19:54:05 +0100

    Draw map from ndarray data

--- src/main.rs
+++ src/main.rs
@@ -12,30 +12,36 @@ fn setup_camera(mut commands: Commands) {
 }
 
 fn setup_map(mut commands: Commands) {
-    let (size_x, size_y) = (100, 100);
+    let (width, height) = (200, 100);
     let sprite_size = 2.;
+
+    let mut cells = ndarray::Array::zeros((width, height));
+    cells[(50, 50)] = 1;
 
     commands
         .spawn((
             Transform::from_xyz(
-                -(size_x as f32 * sprite_size) / 2.,
-                -(size_y as f32 * sprite_size) / 2.,
+                -(width as f32 * sprite_size) / 2.,
+                -(height as f32 * sprite_size) / 2.,
                 0.,
             ),
             Visibility::default(),
         ))
         .with_children(|builder| {
-            for y in 0..=size_y {
-                for x in 0..=size_x {
+            cells
+                .indexed_iter()
+                .for_each(|((x, y), state): ((usize, usize), &u32)| {
                     builder.spawn((
                         Sprite {
                             custom_size: Some(Vec2::splat(sprite_size)),
-                            color: Color::srgba(255., 255., 255., 255.),
+                            color: [
+                                Color::srgba(0., 0., 0., 255.),
+                                Color::srgba(255., 255., 255., 255.),
+                            ][*state as usize],
                             ..default()
                         },
                         Transform::from_xyz(sprite_size * x as f32, sprite_size * y as f32, 0.),
                     ));
-                }
-            }
+                });
         });
 }
