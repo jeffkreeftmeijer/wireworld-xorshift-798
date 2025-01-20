a999c0ce23f587c5e25384ed105b13d9e9889889
Jeff Kreeftmeijer <jeff@kreeft.me>
2025-01-20 20:59:23 +0100

    Set background color and only draw non-zero cells

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ fn main() {
             }),
             ..default()
         }))
+        .insert_resource(ClearColor(Color::srgb(0., 0., 0.)))
         .add_systems(Startup, (setup_camera, setup_map))
         .run();
 }
@@ -33,23 +34,25 @@ fn setup_map(mut commands: Commands) {
             Visibility::default(),
         ))
         .with_children(|builder| {
-            state
-                .cells
-                .indexed_iter()
-                .for_each(|((y, x), state): ((usize, usize), &u8)| {
-                    builder.spawn((
-                        Sprite {
-                            custom_size: Some(Vec2::splat(SCALE)),
-                            color: [
-                                Color::srgba(0., 0., 0., 255.),
-                                Color::srgba(0., 0., 255., 255.),
-                                Color::srgba(255., 255., 255., 255.),
-                                Color::srgba(255., 255., 0., 255.),
-                            ][*state as usize],
-                            ..default()
-                        },
-                        Transform::from_xyz(SCALE * x as f32, -SCALE * y as f32, 0.),
-                    ));
-                });
+            state.cells.indexed_iter().for_each(
+                |((y, x), state): ((usize, usize), &u8)| match state {
+                    0 => (),
+                    _ => {
+                        builder.spawn((
+                            Sprite {
+                                custom_size: Some(Vec2::splat(SCALE)),
+                                color: [
+                                    Color::srgba(0., 0., 0., 255.),
+                                    Color::srgba(0., 0., 255., 255.),
+                                    Color::srgba(255., 255., 255., 255.),
+                                    Color::srgba(255., 255., 0., 255.),
+                                ][*state as usize],
+                                ..default()
+                            },
+                            Transform::from_xyz(SCALE * x as f32, -SCALE * y as f32, 0.),
+                        ));
+                    }
+                },
+            );
         });
 }
